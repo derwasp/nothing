18e09f25411fb8b373bca751b4af1e6311cef282
derwasp <nbcwasp@gmail.com>
2019-06-09 17:58:07 +0200

    bruteforce

--- pt1/week3/BruteCollinearPoints.java
+++ pt1/week3/BruteCollinearPoints.java
@@ -1,13 +1,90 @@
-public class BruteCollinearPoints {
-   public BruteCollinearPoints(Point[] points) {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+
+public class BruteCollinearPoints {
+    ArrayList<LineSegment> lineSegments;
+
+    public BruteCollinearPoints(Point[] points) {
+       if (points == null)
+           throw new java.lang.IllegalArgumentException();
+
+       for (int i = 0; i < points.length; i++) {
+           Point p = points[i];
+           if (p == null)
+               throw new IllegalArgumentException();
+           for (int j = i + 1; j < points.length; j++) {
+               if (points[i].slopeTo(points[j]) == Double.NEGATIVE_INFINITY) {
+                   throw new IllegalArgumentException();
+               }
+           }
+       }
+
+       lineSegments = new ArrayList<>();
+
+       for (int i1 = 0; i1 < points.length; i1++) {
+           Point p1 = points[i1];
+           for (int i2 = 0; i2 < points.length; i2++) {
+               Point p2 = points[i2];
+               for (int i3 = 0; i3 < points.length; i3++) {
+                   Point p3 = points[i3];
+                   for (int i4 = 0; i4 < points.length; i4++) {
+                       Point p4 = points[i4];
+
+                       double p_q = p1.slopeTo(p2);
+                       double p_r = p1.slopeTo(p3);
+                       double p_s = p1.slopeTo(p4);
+                       if (p_q == p_r && p_q == p_s) {
+
+                           if (p1.compareTo(p2) < 0
+                              && p2.compareTo(p3) < 0
+                              && p3.compareTo(p4) < 0)
+
+                            lineSegments.add(new LineSegment(p1, p4));
+                       }
+                   }
+               }
+           }
+       }
    }
 
-   public           int numberOfSegments()     {
-       return 0;
+   public int numberOfSegments()     {
+       return lineSegments.size();
    }
 
     public LineSegment[] segments() {
-       return null;
+       return lineSegments.toArray(new LineSegment[0]);
    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
