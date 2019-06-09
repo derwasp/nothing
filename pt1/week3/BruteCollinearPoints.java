a1beccdf5e376a0a435e8cb2bc5892b647224125
derwasp <nbcwasp@gmail.com>
2019-06-09 19:40:18 +0200

    correct brute styling

--- pt1/week3/BruteCollinearPoints.java
+++ pt1/week3/BruteCollinearPoints.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 
 public class BruteCollinearPoints {
-    ArrayList<LineSegment> lineSegments;
+    private final ArrayList<LineSegment> lineSegments;
 
     public BruteCollinearPoints(Point[] points) {
        if (points == null)
@@ -15,12 +15,16 @@ public class BruteCollinearPoints {
            Point p = points[i];
            if (p == null)
                throw new IllegalArgumentException();
-           for (int j = i + 1; j < points.length; j++) {
-               if (points[i].slopeTo(points[j]) == Double.NEGATIVE_INFINITY) {
-                   throw new IllegalArgumentException();
-               }
-           }
        }
+
+        for (int i = 0; i < points.length; i++) {
+            Point p = points[i];
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].slopeTo(points[j]) == Double.NEGATIVE_INFINITY) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
 
        lineSegments = new ArrayList<>();
 
@@ -33,10 +37,10 @@ public class BruteCollinearPoints {
                    for (int i4 = 0; i4 < points.length; i4++) {
                        Point p4 = points[i4];
 
-                       double p_q = p1.slopeTo(p2);
-                       double p_r = p1.slopeTo(p3);
-                       double p_s = p1.slopeTo(p4);
-                       if (p_q == p_r && p_q == p_s) {
+                       double pQ = p1.slopeTo(p2);
+                       double pR = p1.slopeTo(p3);
+                       double pS = p1.slopeTo(p4);
+                       if (pQ == pR && pQ == pS) {
 
                            if (p1.compareTo(p2) < 0
                               && p2.compareTo(p3) < 0
@@ -50,12 +54,13 @@ public class BruteCollinearPoints {
        }
    }
 
-   public int numberOfSegments()     {
+   public int numberOfSegments() {
        return lineSegments.size();
    }
 
     public LineSegment[] segments() {
-       return lineSegments.toArray(new LineSegment[0]);
+
+        return lineSegments.toArray(new LineSegment[0]);
    }
 
     public static void main(String[] args) {
